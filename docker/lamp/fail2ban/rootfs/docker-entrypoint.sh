18c60df452c2309a8dcb23daaf26e308818b5c39
Alexey Tsarev <Tsarev.Alexey@gmail.com>
2017-05-09 23:24:50 +0400

    Fail2Ban: loglevel to DEBUG. Tengine: fixed logrotate conf permission

--- docker/lamp/fail2ban/rootfs/docker-entrypoint.sh
+++ docker/lamp/fail2ban/rootfs/docker-entrypoint.sh
@@ -1,32 +1,46 @@
 #!/usr/bin/env sh
 
-# set -x
 
-#LIST_FILES_TO_PROCESS=/etc/fail2ban/paths-overrides.local
-#
-#if [ -f "$LIST_FILES_TO_PROCESS" ]; then
-#    while read LINE
-#    do
-#        SPLITTED=(`echo "$LINE" | tr ";" "\n"`)
-#        F=${SPLITTED[2]}
-#
-#        if [ -n "$F" ]; then
-#            echo "$F" | grep "*" > /dev/null
-#
-#            if [ "$?" -ne "0" ] && [ ! -f "$F" ] ; then
-#                echo "Create the file: $F"
-#                touch "$F"
-#                chmod a=rw "$F"
-#            fi
-#        fi
-#    done < "$LIST_FILES_TO_PROCESS"
-#fi
+# $1 - (string) message
+# $2 - (boolean, default true) print date/time
+# $3 - (boolean, default true) print end of line
+log() {
+    if [ "$2" == false ]; then
+        MSG=
+    else
+        MSG="`date '+%Y-%m-%d %H:%M:%S,%3N %Z'` - "
+    fi
 
+    MSG=${MSG}${1}
 
+    if [ "$3" == false ]; then
+        ECHO_OPT=-n
+    else
+        ECHO_OPT=
+    fi
+
+    if [ ${LOG_ECHO} == true ]; then
+        echo ${ECHO_OPT} "$MSG"
+    fi
+
+    echo ${ECHO_OPT} "$MSG" >> "$LOG_FILE"
+}
+
+if [ -n "$TERM" ]; then
+    LOG_ECHO=true
+else
+    LOG_ECHO=false
+fi
+
+LOG_FILE=/var/log/fail2ban/fail2ban.log
+
+log "Fail2Ban starter script. Insert kernel modules"
 modprobe iptable_filter
 modprobe ip6table_filter
 
-
 fail2ban-server -f &
-trap "kill $!" EXIT
+trap "log 'Trapping'; kill $!; log 'Trapped'" EXIT
+
+log "Fail2Ban Server started"
+
 wait
