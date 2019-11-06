e97f4cb3bf22c411e9f73a89d19b3e724037eeba
Alexey Tsarev <Tsarev.Alexey@gmail.com>
2019-11-06 12:55:55 +0400

    Added "--no-cache" to "docker-compose build"

--- build_and_push_all.sh
+++ build_and_push_all.sh
@@ -5,26 +5,28 @@ set -x
 
 ./mark_sh_exec.sh
 
+docker system prune --all --force
+
 ./build_base_image.sh
 docker push alexeysofree/centos7-base:latest
 
 cd lamp
-docker-compose build
+docker-compose build --no-cache
 docker-compose push
 cd -
 
 cd p2p
-docker-compose build
+docker-compose build --no-cache
 docker-compose push
 cd -
 
 cd iot
-docker-compose build
+docker-compose build --no-cache
 docker-compose push
 cd -
 
 cd video_surveillance
-docker-compose build
+docker-compose build --no-cache
 docker-compose push
 cd -
 
