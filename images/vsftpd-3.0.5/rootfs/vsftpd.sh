d0c0aa0a5ef35869d90226ed92ee0b428168e8cf
Alexey Tsarev <Tsarev.Alexey@gmail.com>
2026-02-09 00:43:19 +0400

    vsftpd: 3.0.3 -> 3.0.5

--- images/vsftpd-3.0.5/rootfs/vsftpd.sh
+++ images/vsftpd-3.0.5/rootfs/vsftpd.sh
@@ -3,6 +3,8 @@
 set -e
 #set -x
 
+ETC_CONF_FILE="/etc/vsftpd.conf"
+
 CONF_FILE="/etc/vsftpd/vsftpd.conf"
 CONF_DIR="$(dirname "${CONF_FILE}")"
 
@@ -15,7 +17,13 @@ if [ ! -f "${CONF_FILE}" ] && [ -f "${DEFAULT_CONF_FILE}" ]; then
     rm -rf "${CONF_DIR:?}"/*
     mkdir -p "${CONF_DIR}"
 
-    cp -a -r "${DEFAULT_CONF_DIR}"/* "${CONF_DIR}"
+    cp -a -r -v "${DEFAULT_CONF_DIR}"/* "${CONF_DIR}"
+fi
+
+if [ -f "${ETC_CONF_FILE}" ] && [ ! -h "${ETC_CONF_FILE}" ]; then
+    echo "Symlink ${ETC_CONF_FILE} -> ${CONF_FILE}"
+    mv "${ETC_CONF_FILE}" "${ETC_CONF_FILE}.orig"
+    ln -s "${CONF_FILE}" "${ETC_CONF_FILE}"
 fi
 
 exec vsftpd
