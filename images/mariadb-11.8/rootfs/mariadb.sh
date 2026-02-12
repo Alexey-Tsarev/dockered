1238416d2953268b1e10fd0f8dc684a55e5461b0
Alexey Tsarev <Tsarev.Alexey@gmail.com>
2026-02-12 20:55:48 +0400

    fix all images: completed: docker compose push
    fail2ban 0.11.2 -> 1.1.0
    mariadb 10.11 -> 11.8
    blynk-server: base image: debian 11 -> 13
    ivideon: disable build
    shadowsocks: base image: debian 11 -> 12
    toolbox: uv 0.10.0 -> 0.10.2

--- images/mariadb-11.8/rootfs/mariadb.sh
+++ images/mariadb-11.8/rootfs/mariadb.sh
@@ -5,18 +5,25 @@
 USAGE="Usage: ${0} [build]"
 
 # Cfg
-MYSQL_DB_DIR="/var/lib/mysql"
-MYSQL_LOG_DIR="/var/log/mysql"
-MYSQL_RUN_DIR="/var/run/mysql"
-MYSQL_CNF_DIR="/etc/mysql"
-MYSQL_DB_BUILD_DIR="${MYSQL_DB_DIR}_build"
-MYSQL_CNF_BUILD_DIR="${MYSQL_CNF_DIR}_build"
-MYSQL_PID_FILE="${MYSQL_RUN_DIR}/mysql.pid"
-MYSQL_SOCK_FILE="${MYSQL_RUN_DIR}/mysql.sock"
-MYSQL_USER="${MYSQL_USER:-mysql}"
-MYSQL_MAX_CONNECTIONS="${MYSQL_MAX_CONNECTIONS:-551}"
 MYSQL_BIND_ADDRESS="${MYSQL_BIND_ADDRESS:-127.0.0.1}"
 MYSQL_LISTEN_PORT="${MYSQL_LISTEN_PORT:-3306}"
+MYSQL_USER="${MYSQL_USER:-mysql}"
+
+MYSQL_MAX_CONNECTIONS="${MYSQL_MAX_CONNECTIONS:-551}"
+
+MYSQL_CNF_DIR="${MYSQL_CNF_DIR:-/etc/mysql}"
+MYSQL_DB_DIR="${MYSQL_DB_DIR:-/var/lib/mysql}"
+MYSQL_LOG_DIR="${MYSQL_LOG_DIR:-/var/log/mysql}"
+MYSQL_RUN_DIR="${MYSQL_RUN_DIR:-/run/mysqld}"
+
+MYSQL_GENERAL_LOG_FILE="${MYSQL_GENERAL_LOG_FILE:-${MYSQL_LOG_DIR}/mysql.log}"
+MYSQL_SLOW_QUERY_FILE="${MYSQL_SLOW_QUERY_FILE:-${MYSQL_LOG_DIR}/mysql-slow.log}"
+MYSQL_LOG_ERROR="${MYSQL_LOG_ERROR:-${MYSQL_LOG_DIR}/mysql-error.log}"
+
+MYSQL_CNF_BUILD_DIR="${MYSQL_CNF_BUILD_DIR:-${MYSQL_CNF_DIR}_build}"
+MYSQL_DB_BUILD_DIR="${MYSQL_DB_BUILD_DIR:-${MYSQL_DB_DIR}_build}"
+MYSQL_PID_FILE="${MYSQL_PID_FILE:-${MYSQL_RUN_DIR}/mysqld.pid}"
+MYSQL_SOCK_FILE="${MYSQL_SOCK_FILE:-${MYSQL_RUN_DIR}/mysqld.sock}"
 # End Cfg
 
 if [ -n "$1" ]; then
@@ -47,7 +54,7 @@ fi
 
 mkdir -p "${MYSQL_CNF_DIR}" "${MYSQL_DB_DIR}" "${MYSQL_LOG_DIR}" "${MYSQL_RUN_DIR}"
 
-if [ -d "${MYSQL_DB_BUILD_DIR}" ] && [ -z "$(ls -A ${MYSQL_DB_DIR})" ]; then
+if [ -d "${MYSQL_DB_BUILD_DIR}" ] && [ -z "$(ls -A "${MYSQL_DB_DIR}")" ]; then
     echo -n "Moving DB data... "
     mv "${MYSQL_DB_BUILD_DIR}"/* "${MYSQL_DB_DIR}"
     ln -s "${MYSQL_SOCK_FILE}" "${MYSQL_DB_DIR}"
@@ -55,7 +62,7 @@ if [ -d "${MYSQL_DB_BUILD_DIR}" ] && [ -z "$(ls -A ${MYSQL_DB_DIR})" ]; then
     echo "done"
 fi
 
-if [ -d "${MYSQL_CNF_BUILD_DIR}" ] && [ -z "$(ls -A ${MYSQL_CNF_DIR})" ]; then
+if [ -d "${MYSQL_CNF_BUILD_DIR}" ] && [ -z "$(ls -A "${MYSQL_CNF_DIR}")" ]; then
     echo -n "Moving cnf data... "
     mv "${MYSQL_CNF_BUILD_DIR}"/* "${MYSQL_CNF_DIR}"
     rm -rf "${MYSQL_CNF_BUILD_DIR}"
@@ -80,11 +87,16 @@ fi
 date
 echo "Run MySQL:"
 set -x
-exec mariadbd --user="${MYSQL_USER}" \
-              --plugin-dir=/usr/lib64/mysql/plugin \
-              --datadir="${MYSQL_DB_DIR}" \
-              --socket="${MYSQL_SOCK_FILE}" \
-              --pid-file="${MYSQL_PID_FILE}" \
-              --bind-address="${MYSQL_BIND_ADDRESS}" \
-              --port="${MYSQL_LISTEN_PORT}" \
-              --max-connections="${MYSQL_MAX_CONNECTIONS}" \
+exec \
+  mariadbd \
+    --user="${MYSQL_USER}" \
+    --plugin-dir=/usr/lib64/mysql/plugin \
+    --datadir="${MYSQL_DB_DIR}" \
+    --socket="${MYSQL_SOCK_FILE}" \
+    --pid-file="${MYSQL_PID_FILE}" \
+    --bind-address="${MYSQL_BIND_ADDRESS}" \
+    --port="${MYSQL_LISTEN_PORT}" \
+    --max-connections="${MYSQL_MAX_CONNECTIONS}" \
+    --general-log-file="${MYSQL_GENERAL_LOG_FILE}" \
+    --log-slow-query-file="${MYSQL_SLOW_QUERY_FILE}" \
+    --log-error="${MYSQL_LOG_ERROR}"
