d56ad05d00b51738f6986105235d1bfcbe7acb42
Alexey Tsarev <Tsarev.Alexey@gmail.com>
2026-03-16 23:38:31 +0400

    add: dante-server:1.4.4; slipstream-rust:0.1.0

---
+++ images/dante-server-1.4.4/rootfs/dante-server.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -a
+EXTERNAL="$(ip route show default | awk '{print $5}')"
+set +a
+
+template_envsubst.sh /opt/dante/conf/template-orig /opt/dante/conf
+template_envsubst.sh /opt/dante/conf/template      /opt/dante/conf
+
+echo "Config file:"
+cat /opt/dante/conf/sockd.conf
+echo
+
+set -x
+
+#strace -o /opt/dante/log/strace.txt ./sockd -f /opt/dante/conf/sockd.conf
+exec ./sockd -f /opt/dante/conf/sockd.conf
---
+++ images/slipstream-rust-0.1.0/rootfs/slipstream-server.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -x
+
+eval exec ./slipstream-server \
+    --dns-listen-port "${DNS_LISTEN_PORT:-53}" \
+    --target-address "${TARGET_ADDRESS:-127.0.0.1:1080}" \
+    --domain "${DOMAIN:-example.com}" \
+    --cert "${CERT:-conf/cert.pem}" \
+    --key "${KEY:-conf/key.pem}" \
+    --reset-seed "${RESET_SEED:-conf/reset-seed}" \
+    "${OPTS}"
