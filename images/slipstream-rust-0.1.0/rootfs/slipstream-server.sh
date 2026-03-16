09e095481bf930399f076801128c1da249274136
Alexey Tsarev <Tsarev.Alexey@gmail.com>
2026-03-17 00:49:33 +0400

    add: slipstream-rust: DNS_LISTEN_HOST

--- images/slipstream-rust-0.1.0/rootfs/slipstream-server.sh
+++ images/slipstream-rust-0.1.0/rootfs/slipstream-server.sh
@@ -3,6 +3,7 @@
 set -x
 
 eval exec ./slipstream-server \
+    --dns-listen-host "${DNS_LISTEN_HOST:-0.0.0.0}" \
     --dns-listen-port "${DNS_LISTEN_PORT:-53}" \
     --target-address "${TARGET_ADDRESS:-127.0.0.1:1080}" \
     --domain "${DOMAIN:-example.com}" \
