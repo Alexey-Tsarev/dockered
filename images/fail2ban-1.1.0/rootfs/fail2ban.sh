7d13d595c805322414eca214e4445e43db2e4906
Alexey Tsarev <Tsarev.Alexey@gmail.com>
2026-04-14 23:19:04 +0400

    update: fail2ban; change failregex for xrdp

--- images/fail2ban-1.1.0/rootfs/fail2ban.sh
+++ images/fail2ban-1.1.0/rootfs/fail2ban.sh
@@ -4,9 +4,6 @@ PS4='$(date "+%Y-%m-%d %H:%M:%S,%3N") '
 
 # set -x
 
-modprobe iptable_filter
-modprobe ip6table_filter
-
 rm -f /run/fail2ban/fail2ban.sock
 rm -f /run/fail2ban/fail2ban.pid
 #rm -f /var/lib/fail2ban/fail2ban.sqlite3
